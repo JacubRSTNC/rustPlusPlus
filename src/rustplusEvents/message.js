5c879aa2dff5d442fe0f11921db9f535e9dfa964
Jacub <38807049+JacubRSTNC@users.noreply.github.com>
2022-03-04 17:30:46 +0100

    Update message.js

--- src/rustplusEvents/message.js
+++ src/rustplusEvents/message.js
@@ -19,7 +19,7 @@ module.exports = {
             }
             else if (message.broadcast.hasOwnProperty('teamMessage')) {
                 /* Let command handler handle the potential command */
-                let handled = await CommandHandler.inGameCommandHandler(rustplus, client, message);
+                let handled = CommandHandler.inGameCommandHandler(rustplus, client, message);
                 if (!handled) {
                     TeamChatHandler(rustplus, client, message.broadcast.teamMessage.message);
                 }
